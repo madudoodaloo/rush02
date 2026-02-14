a3d01c12e57397c03ef715130af90ad76b1d22ef
masilva- <masilva-@student.42lisboa.com>
2026-02-14 18:46:27 +0000

    header.h

---
+++ init_dict.c
@@ -0,0 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_dict.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: masilva-@student.42lisboa.com <masilva-    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/14 18:20:09 by masilva-@st       #+#    #+#             */
+/*   Updated: 2026/02/14 18:20:27 by masilva-@st      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//Your program must parse the dictionary given as a resource to the project. The
+//values inside it must be used to print the result. These values can be modified.
---
+++ main.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+///*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: masilva-@student.42lisboa.com <masilva-    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/14 18:19:02 by masilva-@st       #+#    #+#             */
+/*   Updated: 2026/02/14 18:19:08 by masilva-@st      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//• Your program can take up to 2 arguments:
+//◦ If there is only one argument, it is the number that you need to convert.
+//◦ If there are two arguments, the first argument is the new reference dictionary
+//and the second argument is the number that you need to convert.
+//• If the argument representing the number is not a valid and positive integer, your
+//program must output "Error" followed by a newline.
+
+
+int main(int ac, )
+{
+    fd = open(av[1]);
+    if (fd = -1)
+        return (ERROR)
+    read()
+}
